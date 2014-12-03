288e83377080128fc71fee993f681ae0a9b0ec75
Antonio Calanducci <acaland@Antonios-MacBook-Air.local>
2014-12-03 15:15:55 +0100

    adding validate method

--- MyToDoListMVC-Model/app/models/todo.js
+++ MyToDoListMVC-Model/app/models/todo.js
@@ -14,6 +14,12 @@ exports.definition = {
 	extendModel: function(Model) {
 		_.extend(Model.prototype, {
 			// extended functions and properties go here
+			validate: function(attrs) {
+                                if (!attrs.title || attrs.title.length <= 0) {
+                                        Ti.API.info("nessun titolo");
+                                        return "Error: No title!"
+                                }
+                        }
 		});
 
 		return Model;
